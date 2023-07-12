55ea4641bbb67621144bbc33fd47c79f1c159b69
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-07-12 19:56:07 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+export default {
+    JWT_SECRET : "patnanit",
+    EMAIL: "steve.franecki@ethereal.email", 
+    PASSWORD : "sMf46xCzrvdrxvuagc",
+    ATLAS_URI: "mongodb+srv://nitpatna:nit123patna@nitpatna.ncyqvm9.mongodb.net/?retryWrites=true&w=majority",
+    port: process.env.PORT || 8080,
+    // mongoURI: process.env.MONGO_URI || 'mongodb+srv://Abhi6722:Abhi6722@researchrover.ckwzho3.mongodb.net/?retryWrites=true&w=majority',
+    smtpHost: process.env.SMTP_HOST || 'smtp.hostinger.com',
+    smtpPort: process.env.SMTP_PORT || 465,
+    smtpUser: process.env.SMTP_USER || 'hackshark@heistat49.com',
+    smtpPassword: process.env.SMTP_PASSWORD || 'Hack@shark11',
+}
