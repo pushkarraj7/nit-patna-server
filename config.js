24957af869851ee48031f9d003f9a41ee8480e83
PUSHKAR RAJ <86008182+pushkarraj7@users.noreply.github.com>
2023-09-08 22:45:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 export default {
     JWT_SECRET : "patnanit",
-    EMAIL: "steve.franecki@ethereal.email", 
-    PASSWORD : "sMf46xCzrvdrxvuagc",
-    ATLAS_URI: "mongodb+srv://nitpatna:nit123patna@nitpatna.ncyqvm9.mongodb.net/?retryWrites=true&w=majority",
+    ATLAS_URI: "mongodb+srv://pushkar:pushkar@nitpatna.nzqlwld.mongodb.net/",
     port: process.env.PORT || 8080,
-    // mongoURI: process.env.MONGO_URI || 'mongodb+srv://rajpushkar556:rajpushkar556@researchrover.ckwzho3.mongodb.net/?retryWrites=true&w=majority',
     smtpHost: process.env.SMTP_HOST || 'smtp.hostinger.com',
     smtpPort: process.env.SMTP_PORT || 465,
-    smtpUser: process.env.SMTP_USER || 'hackshark@heistat49.com',
-    smtpPassword: process.env.SMTP_PASSWORD || 'Hack@shark11',
+    smtpUser: process.env.SMTP_USER || 'youtube@campuscareline.co.in',
+    smtpPassword: process.env.SMTP_PASSWORD || 'Youtube@11',
 }
